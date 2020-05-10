365816916ad02eb9248ba7726c64e44394dc0a00
xin.fang <xin.fang@ucarinc.com>
2020-05-10 22:58:49 +0800

    分布式微服务数据平台初次提交

---
+++ dp-order/src/main/java/com/fx/order/DpOrderApplication.java
@@ -0,0 +1,13 @@
+package com.fx.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpOrderApplication.class, args);
+    }
+
+}
